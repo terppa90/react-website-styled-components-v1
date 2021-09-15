242b1632804e70daf9e38cd69e7ecc939a257b3a
Tero Kaihula <N0465@student.jamk.fi>
2021-09-15 17:45:51 +0300

    react tutorial

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import { Button } from '../../globalStyles';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin,
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterSubscription,
+  FooterSubHeading,
+  FooterSubtext,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  SocialIcon,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink,
+} from './Footer.elements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterSubscription>
+        <FooterSubHeading>
+          Join out exclusive membership to receive the latest news and trends
+        </FooterSubHeading>
+        <FooterSubtext>You can unsubscribe at anytime</FooterSubtext>
+        <Form>
+          <FormInput name="email" type="email" placeholder="Your Email" />
+          <Button fontBig>Subscribe</Button>
+        </Form>
+      </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to="/sign-up">Hot it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to="/sign-up">Hot it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+        <FooterLinksWrapper>
+          <FooterLinkItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to="/sign-up">Hot it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinkItems>
+          <FooterLinkItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to="/sign-up">Hot it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinkItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
+      <SocialMedia>
+        <SocialMediaWrap>
+          <SocialLogo to="/">
+            <SocialIcon />
+            ULTRA
+          </SocialLogo>
+          <WebsiteRights>ULTRA © 2020</WebsiteRights>
+          <SocialIcons>
+            <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+              <FaFacebook />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+              <FaInstagram />
+            </SocialIconLink>
+            <SocialIconLink
+              href={
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              rel="noopener noreferrer"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <FaYoutube />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+              <FaTwitter />
+            </SocialIconLink>
+            <SocialIconLink href="/" target="_blank" aria-label="LinkedIn">
+              <FaLinkedin />
+            </SocialIconLink>
+          </SocialIcons>
+        </SocialMediaWrap>
+      </SocialMedia>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,67 @@
+export const homeObjOne = {
+  lightBg: false,
+  primary: true,
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description:
+    'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: true,
+  topLine: 'Margeting Agency',
+  img: require('../../images/svg-1.svg').default,
+  alt: 'Image',
+  start: '',
+};
+
+export const homeObjTwo = {
+  lightBg: true,
+  primary: false,
+  imgStart: 'start',
+  lightTopLine: false,
+  lightTextDesc: false,
+  buttonLabel: 'Get Started',
+  description:
+    'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: false,
+  topLine: 'Margeting Agency',
+  img: require('../../images/profile.jpg').default,
+  alt: 'Image',
+  start: 'true',
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  primary: true,
+  imgStart: '',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description:
+    'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: true,
+  topLine: 'Margeting Agency',
+  img: require('../../images/svg-2.svg').default,
+  alt: 'Image',
+  start: '',
+};
+
+export const homeObjFour = {
+  lightBg: true,
+  primary: false,
+  imgStart: 'start',
+  lightTopLine: true,
+  lightTextDesc: true,
+  buttonLabel: 'Get Started',
+  description:
+    'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+  headLine: 'Lead Generation Specialist for Online Businesses',
+  lightText: true,
+  topLine: 'Margeting Agency',
+  img: require('../../images/svg-3.svg').default,
+  alt: 'Image',
+  start: 'true',
+};
